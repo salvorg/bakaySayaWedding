5b97546d49a9d78c5abe9b1c2ac4e37209bef2cf
Almaz Balbaev <kujitengu@gmail.com>
2023-11-13 00:38:36 +0600

    - changed JSX logic - changed classes - added animation

--- src/components/ObserverCarousel.js
+++ src/components/ObserverCarousel.js
@@ -6,26 +6,38 @@ gsap.registerPlugin(Observer);
 
 const ObserverCarousel = () => {
     const [state, setState] = useState(0);
-
+    const [isAnimating, setIsAnimating] = useState(false);
+    const headers = ["Scroll down", "Animated with GSAP", "GreenSock", "Animation platform", "Keep scrolling"]
     const sections = document.querySelectorAll("section");
+    const images = document.querySelectorAll(".bg");
 
     const moveDown = (state) => {
         if (state > 0) {
-            console.log('trying to scroll up');
             setState(state - 1);
         }
     }
 
     const moveUp = (state) => {
-        if (state < 5) {
-            console.log('trying to scroll down');
+        if (state < 4) {
             setState(state + 1);
         }
     }
 
     useEffect(() => {
-        gsap.set(sections, {visibility: "hidden"});
-        gsap.set(sections[state -1], {visibility: "visible"});
+        const tl = gsap.timeline();
+
+        if (!isAnimating) {
+            setIsAnimating(true);
+
+            tl.to(images[state], {opacity: 0, duration: 0.1})
+                .set(sections, {visibility: "hidden"})
+                .set(sections[state], {visibility: "visible"})
+                .fromTo(images[state], {opacity: 0}, {opacity: 1, duration: 0.6})
+                .call(() => setIsAnimating(false));
+
+            gsap.set(sections, {visibility: "hidden"});
+            gsap.set(sections[state], {visibility: "visible"});
+        }
     }, [state]);
 
     Observer.create({
@@ -36,8 +48,6 @@ const ObserverCarousel = () => {
         wheelSpeed: -1,
     });
 
-    console.log(state);
-
     return (
         <main>
             <header>
@@ -46,51 +56,65 @@ const ObserverCarousel = () => {
                     <a href="https://codepen.io/BrianCross/pen/PoWapLP">Original By Brian</a>
                 </div>
             </header>
-            <section className="first">
-                <div className="outer">
-                    <div className="inner">
-                        <div className="bg one">
-                            <h2 className="section-heading">Scroll down</h2>
+            {Array.from({length: 5}, (_, index) => (
+                <section
+                    key={index}
+                    className={`section section-${index + 1}`}
+                >
+                    <div className="outer">
+                        <div className="inner">
+                            <div className="bg">
+                                <h2 className="section-heading">{headers[index]}</h2>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </section>
-            <section className="second">
-                <div className="outer">
-                    <div className="inner">
-                        <div className="bg">
-                            <h2 className="section-heading">Animated with GSAP</h2>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            <section className="third">
-                <div className="outer">
-                    <div className="inner">
-                        <div className="bg">
-                            <h2 className="section-heading">GreenSock</h2>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            <section className="fourth">
-                <div className="outer">
-                    <div className="inner">
-                        <div className="bg">
-                            <h2 className="section-heading">Animation platform</h2>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            <section className="fifth">
-                <div className="outer">
-                    <div className="inner">
-                        <div className="bg">
-                            <h2 className="section-heading">Keep scrolling</h2>
-                        </div>
-                    </div>
-                </div>
-            </section>
+                </section>
+            ))}
+            {/*<section className="section section-1">*/}
+            {/*    <div className="outer">*/}
+            {/*        <div className="inner">*/}
+            {/*            <div className="bg one">*/}
+            {/*                <h2 className="section-heading">Scroll down</h2>*/}
+            {/*            </div>*/}
+            {/*        </div>*/}
+            {/*    </div>*/}
+            {/*</section>*/}
+            {/*<section className="section section-2">*/}
+            {/*    <div className="outer">*/}
+            {/*        <div className="inner">*/}
+            {/*            <div className="bg">*/}
+            {/*                <h2 className="section-heading"></h2>*/}
+            {/*            </div>*/}
+            {/*        </div>*/}
+            {/*    </div>*/}
+            {/*</section>*/}
+            {/*<section className="section section-3">*/}
+            {/*    <div className="outer">*/}
+            {/*        <div className="inner">*/}
+            {/*            <div className="bg">*/}
+            {/*                <h2 className="section-heading">GreenSock</h2>*/}
+            {/*            </div>*/}
+            {/*        </div>*/}
+            {/*    </div>*/}
+            {/*</section>*/}
+            {/*<section className="section section-4">*/}
+            {/*    <div className="outer">*/}
+            {/*        <div className="inner">*/}
+            {/*            <div className="bg">*/}
+            {/*                <h2 className="section-heading">Animation platform</h2>*/}
+            {/*            </div>*/}
+            {/*        </div>*/}
+            {/*    </div>*/}
+            {/*</section>*/}
+            {/*<section className="section section-5">*/}
+            {/*    <div className="outer">*/}
+            {/*        <div className="inner">*/}
+            {/*            <div className="bg">*/}
+            {/*                <h2 className="section-heading">Keep scrolling</h2>*/}
+            {/*            </div>*/}
+            {/*        </div>*/}
+            {/*    </div>*/}
+            {/*</section>*/}
         </main>
     );
 };
