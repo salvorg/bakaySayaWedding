a5cb7da5a0769f28d0712d4a7141f4348fe7dab5
Almaz Balbaev <kujitengu@gmail.com>
2023-11-15 18:08:31 +0600

    - added axios

---
+++ src/axios-api.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+const axiosApi = axios.create({
+    baseURL: 'https://weddingapp-655de-default-rtdb.europe-west1.firebasedatabase.app/',
+});
+
+export default axiosApi;
+
