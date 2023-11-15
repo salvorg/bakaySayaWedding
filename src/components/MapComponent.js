bf87d9926d6301e96dff202553988a094ac78afa
Almaz Balbaev <kujitengu@gmail.com>
2023-11-15 18:09:13 +0600

    - added interactive map component

---
+++ src/components/MapComponent.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import "leaflet/dist/leaflet.css";
+
+const MapComponent = () => {
+    const center = [42.824342, 74.642409];
+
+    return (
+        <div style={{ height: '400px', width: '400px', border: '5px solid #fff5' }}>
+            <MapContainer center={center} zoom={14} style={{ height: '100%', width: '100%' }} scrollWheelZoom={false}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={center}>
+                    <Popup>
+                        A pretty CSS3 popup. <br /> Easily customizable.
+                    </Popup>
+                </Marker>
+            </MapContainer>
+        </div>
+    );
+};
+
+export default MapComponent;
