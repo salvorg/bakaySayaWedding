61d651f7b305cb846a6edec936da6b63ed585797
Almaz Balbaev <kujitengu@gmail.com>
2023-11-15 20:07:27 +0600

    - added react-router-dom and routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './styles/global.scss';
 import React from "react";
 import ObserverCarousel from "./components/ObserverCarousel";
+import {Route, Routes} from "react-router-dom";
+import GuestList from "./components/GuestList";
 
 function App() {
   return (
     <>
-        <ObserverCarousel />
+        <Routes>
+            <Route path="/" element={<ObserverCarousel />} />
+            <Route path="/list" element={<GuestList />} />
+        </Routes>
+
     </>
   );
 }
